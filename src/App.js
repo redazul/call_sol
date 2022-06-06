51a40bc6a5f650a9ad0128903ce88bf08f558fc0
redazul <azuldevgames@gmail.com>
2022-06-06 15:41:57 -0500

    update

--- src/App.js
+++ src/App.js
@@ -23,6 +23,74 @@ const {
 
 class App extends Component {
 
+  initialize(tracker, user, authority, counter, trackerProgramId){
+    return new TransactionInstruction({
+      keys: [
+        {
+          pubkey: tracker,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: user,
+          isSigner: true,
+          isWritable: false,
+        },
+        {
+          pubkey: authority,
+          isSigner: false,
+          isWritable: false,
+        },
+        {
+          pubkey: counter,
+          isSigner: false,
+          isWritable: false,
+        },
+        {
+          pubkey: SystemProgram.programId,
+          isSigner: false,
+          isWritable: false,
+        },
+      ],
+      data: Buffer.from(new Uint8Array([0])),
+      programId: trackerProgramId,
+    });
+  };
+
+  increment(tracker, user, authority, counter, counterProgramId, trackerProgramId){
+    return new TransactionInstruction({
+      keys: [
+        {
+          pubkey: tracker,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: user,
+          isSigner: true,
+          isWritable: false,
+        },
+        {
+          pubkey: counterProgramId,
+          isSigner: false,
+          isWritable: false,
+        },
+        {
+          pubkey: counter,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: authority,
+          isSigner: false,
+          isWritable: false,
+        },
+      ],
+      data: Buffer.from(new Uint8Array([1])),
+      programId: trackerProgramId,
+    });
+  };
+
 
 
 
@@ -208,8 +276,11 @@ async goCall2()
 
   console.log("Auth flow")
 
-  const trackerProgramId = new PublicKey("CeAA9rP7xNJZWgrsPp84TSiQr32KGjh5VYvGSuYk7iSt");
-  const counterProgramId = new PublicKey("EnDET6JzFbb9uAaskPmAde6ooYXzsiRM224ZJ7w2Qw6r");
+  const trackerProgramId = new PublicKey("2VE86vbitM7KsDmwsi8fAYd7MAHUCCUaFWiRJUK7D6C1");
+  const counterProgramId = new PublicKey("EuMLTb1agRSf2hM4eUbZxoNn47y6YW2MTWsyEinP79ks");
+
+  console.log("Tracker Program", trackerProgramId.toBase58());
+  console.log("Counter Program", counterProgramId.toBase58());
   const connection = new Connection("https://api.devnet.solana.com/");
 
   const resp = await window.solana.connect();
@@ -218,8 +289,91 @@ async goCall2()
 
   const phantomPubKey = new PublicKey(resp.publicKey.toString())
 
+  const counter = new Keypair();
+  let counterKey = counter.publicKey;
+
+  let tx = new Transaction();
+
+  if ((await connection.getBalance(phantomPubKey)) < 0.1) {
+    console.log("Requesting Airdrop of 1 SOL...");
+    await connection.requestAirdrop(phantomPubKey, 2e9);
+    console.log("Airdrop received");
+  }
+
+  
+  console.log("Generating new counter address");
+  let createIx = SystemProgram.createAccount({
+    fromPubkey: phantomPubKey,
+    newAccountPubkey: counterKey,
+    /** Amount of lamports to transfer to the created account */
+    lamports: await connection.getMinimumBalanceForRentExemption(40),
+    /** Amount of space in bytes to allocate to the created account */
+    space: 40,
+    /** Public key of the program to assign as the owner of the created account */
+    programId: counterProgramId,
+  });
+  
+  tx.add(createIx);
+
+  const trackerKey = (await PublicKey.findProgramAddress(
+    [phantomPubKey.toBuffer(), counterKey.toBuffer()],
+    trackerProgramId
+  ))[0];
+  const authKey = (await PublicKey.findProgramAddress(
+    [counterKey.toBuffer()],
+    trackerProgramId
+  ))[0];
+
+  console.log(trackerKey.toBase58())
+  console.log(authKey.toBase58())
+
+  let trackerData = await connection.getAccountInfo(trackerKey)
+  if (!trackerData) {
+    console.log("    -> No tracker account found. Creating new tracker account");
+    const initializeIx = this.initialize(
+      trackerKey,
+      phantomPubKey,
+      authKey,
+      counterKey,
+      trackerProgramId
+    );
+    tx.add(initializeIx);
+  }
+
+  console.log("Incrementing counter");
+  const trackerInstruction = this.increment(
+    trackerKey,
+    phantomPubKey,
+    authKey,
+    counterKey,
+    counterProgramId,
+    trackerProgramId
+  )
+  tx.add(trackerInstruction);
+
+  console.log(tx)
+
+  let blockhash = (await connection.getLatestBlockhash("finalized")).blockhash;
+  console.log("recentBlockhash: ", blockhash);
+  tx.recentBlockhash = blockhash;
+  tx.feePayer = phantomPubKey;
+
+  tx.partialSign(counter)
+
+  console.log(tx)
+
+  const { signature } = await window.solana.signAndSendTransaction(tx,{skipPreflight:true});
+  console.log(signature)
+
+  window.open(`https://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
 
+
+}
+
+goCall3()
+{
+  console.log("Here we go escrow")
 }
 
 
@@ -233,7 +387,9 @@ async goCall2()
       <div style={{marginTop:window.screen.availHeight/3}} className='center'>
         <button onClick={this.goCall.bind(this)} className='glow'>Send Counter Transaction</button>
         <div style={{marginTop:40}}></div>
-        <button onClick={this.goCall2.bind(this)} className='glow'>Send Auth Transaction</button>
+        <button onClick={this.goCall2.bind(this)} className='glow'>Send Auth Transaction</button>        
+        <div style={{marginTop:40}}></div>
+        <button onClick={this.goCall3.bind(this)} className='glow'>Init Escrow</button>
         {/* <button onClick={this.goCall.bind(this)} className='glow'>Hello World Program</button> */}
       </div>
     );
